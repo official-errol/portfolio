d43746f93990342704cb160da8dab145390ce658
Errol <109810415+official-errol@users.noreply.github.com>
2025-06-13 11:25:52 +0800

    Update Chat.tsx

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,4 +1,4 @@
-5import React from 'react'
+import React from 'react'
 import { useAuth } from '../context/AuthContext'
 import ChatBox from '../components/ChatBox'
 import LoadingSpinner from '../components/LoadingSpinner'
