36124268a0846c2024f5de7cd273a2a8178a6eb0
Errol <109810415+official-errol@users.noreply.github.com>
2025-06-13 10:01:04 +0800

    Update ChatBox.tsx

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -368,9 +368,9 @@ const ChatBox: React.FC = () => {
           type="submit"
           disabled={!newMessage.trim()}
           className={`h-12 w-12 flex items-center justify-center text-white bg-main rounded-lg cursor-pointer select-none
-            active:translate-y-1 active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+            active:translate-y-1 active:[box-shadow:0_0px_0_0_#6CC832,0_0px_0_0_#9cee69]
             active:border-b-[0px]
-            transition-all duration-150 [box-shadow:0_6px_0_0_#1b6ff8,0_10px_0_0_#1b70f841]
+            transition-all duration-150 [box-shadow:0_6px_0_0_#6CC832,0_10px_0_0_#9cee69]
             border-b border-main ${
               newMessage.trim() ? '' : 'cursor-not-allowed'
             }`}
