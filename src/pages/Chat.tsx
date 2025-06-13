99ae1097ac282c95bcd5df81b420c0a976c06262
Errol <109810415+official-errol@users.noreply.github.com>
2025-06-13 09:56:18 +0800

    Update Chat.tsx

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -86,7 +86,7 @@ const Chat: React.FC = () => {
                         viewBox="0 0 24 24"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
-                        className="w-4 h-4 text-blue-500"
+                        className="w-4 h-4 text-main"
                       >
                         <path
                           fillRule="evenodd"
