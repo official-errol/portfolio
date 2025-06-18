33a06c4fa05e2a8238452e82b2d4946c1ca20365
Errol <109810415+official-errol@users.noreply.github.com>
2025-06-19 06:00:33 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -93,14 +93,14 @@ const Home: React.FC = () => {
     </p>
     <a 
       href="/services" 
-      className="inline-flex items-center px-5 py-3 text-main-dark bg-main rounded-lg cursor-pointer select-none
+      className="inline-flex items-center px-5 py-3 text-xs text-main-dark bg-main rounded-lg cursor-pointer select-none
               active:translate-y-2 active:[box-shadow:0_0px_0_0_#6CC832,0_0px_0_0_#9cee69]
               active:border-b-[1px]
               transition-all duration-150 [box-shadow:0_6px_0_0_#6CC832,0_10px_0_0_#9cee69]
               border-b border-main-dark"
     >
       <ArrowRightIcon className="w-5 h-5 mr-2" />
-      View All Services
+      MY SERVICES
     </a>
   </div>
 </motion.div>
