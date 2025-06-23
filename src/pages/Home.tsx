b186c139073f1187b277c695309249da003baf65
Errol <109810415+official-errol@users.noreply.github.com>
2025-06-23 19:25:30 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,7 +74,7 @@ const Home: React.FC = () => {
             <ArrowRightIcon className="w-4 h-4 mr-2" />
             VIEW PROJECTS
           </a>
-        </motion.div
+        </motion.div>
       </motion.div>
       {/* Services Section */}
       <motion.div
