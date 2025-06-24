85e7af8ab7d85a1bfaf62e7027ca77a2c1748fbb
Errol <109810415+official-errol@users.noreply.github.com>
2025-06-24 17:18:50 +0800

    Update AdminDashboard.tsx

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -55,7 +55,11 @@ const AdminDashboard: React.FC = () => {
 
         <button
           onClick={handleLogout}
-          className="flex items-center gap-3 mt-10 w-full px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+              className="flex items-center gap-3 w-full py-3 px-4 text-xs text-white bg-red-500 rounded-lg cursor-pointer select-none
+                active:translate-y-2 active:[box-shadow:0_0px_0_0_#e11d48,0_0px_0_0_#e11d4866]
+                active:border-b-[0px]
+                transition-all duration-150 [box-shadow:0_6px_0_0_#e11d48,0_10px_0_0_#e11d4866]
+                border-b-[1px] border-red-400"
         >
           <ArrowRightOnRectangleIcon className="h-5 w-5" />
           Logout
