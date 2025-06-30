3c39eb57984a803aeda55d8cbf3da26006883591
Errol <109810415+official-errol@users.noreply.github.com>
2025-06-30 18:23:22 +0800

    Update CommentSection.tsx

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -249,7 +249,7 @@ export const CommentSection: React.FC<{ postId: string }> = ({ postId }) => {
             />
             <button
               onClick={() => addComment(null)}
-              className="self-end bg-main text-sm text-main-dark px-4 py-1 rounded-full hover:bg-main-dark"
+              className="self-end bg-main text-xs text-main-dark px-4 py-2 rounded-full hover:bg-main-dark"
             >
               POST
             </button>
