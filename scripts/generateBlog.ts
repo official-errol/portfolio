77823cdca40ae44a58ddceea060130cea15f2742
Errol <109810415+official-errol@users.noreply.github.com>
2025-07-02 08:19:31 +0800

    Update generateBlog.ts

--- scripts/generateBlog.ts
+++ scripts/generateBlog.ts
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL!,
-  process.env.VITE_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
 )
 
 const generatePost = async () => {
