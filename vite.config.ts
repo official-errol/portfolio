195eaac18a6efe66474a2da23e02e835fbf75bf7
Errol <109810415+official-errol@users.noreply.github.com>
2025-07-11 14:26:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,18 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
     'process.env': {}
+  },
+  server: {
+    proxy: {
+      '/dev-to-api': {
+        target: 'https://dev.to',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/dev-to-api/, ''),
+        headers: {
+          'api-key': import.meta.env.VITE_DEV_TO_API_KEY,
+          'Accept': 'application/vnd.forem.api-v1+json'
+        }
+      }
+    }
   }
 })
