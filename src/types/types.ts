0ffd0089d41666d35a9e97117fb4a2f8de22a147
Errol <109810415+official-errol@users.noreply.github.com>
2025-07-11 15:01:32 +0800

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,20 +1,13 @@
+// src/types.ts
 export interface DevToArticle {
-  id: number
-  title: string
-  description: string
-  published_at: string
-  slug: string
-  path: string
-  url: string
-  comments_count: number
-  public_reactions_count: number
-  body_markdown?: string
-  tag_list: string[]
-  tags: string
+  id: number;
+  title: string;
+  body_markdown?: string; // Article content
+  published_at: string;
   user: {
-    name: string
-    username: string
-    profile_image?: string
-  }
-  cover_image?: string | null
+    name: string;
+    username: string;
+  };
+  tag_list: string[];
+  cover_image?: string | null;
 }
