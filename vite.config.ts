a977a2213c7287679c0f4428bb2f98e679edda06
Errol <109810415+official-errol@users.noreply.github.com>
2025-07-11 14:56:57 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, type ConfigEnv, type UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import type { UserConfigExport } from 'vite'
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
@@ -19,11 +17,11 @@ export default defineConfig(({ mode }) => {
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/dev-to-api/, ''),
           headers: {
-            'api-key': env.VITE_DEV_TO_API_KEY,  // Use env object here
+            'api-key': env.VITE_DEV_TO_API_KEY,
             'Accept': 'application/vnd.forem.api-v1+json'
           }
         }
       }
     }
-  } as UserConfigExport
+  }
 })
