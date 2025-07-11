2c0dfbf1160bb8849a4812e7a0c711fc3af06cea
Errol <109810415+official-errol@users.noreply.github.com>
2025-07-11 14:55:01 +0800

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_DEV_TO_API_KEY: string
+  // Add other environment variables here as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
