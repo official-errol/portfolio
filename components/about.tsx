b22bd1542eec163aae71849dfa51fc029c905a73
Errol <109810415+official-errol@users.noreply.github.com>
2025-07-29 03:05:17 +0800

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,7 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { FolderKanban, Handshake, Laptop2, UsersRound } from "lucide-react";
+import { Laptop2, UsersRound } from "lucide-react";
 import Image from "next/image";
 import { HTMLAttributes } from "react";
 
