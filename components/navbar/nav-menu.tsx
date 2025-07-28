b9de77ebbb84cc2dff23bb5c2cc1775b852615d7
Errol <109810415+official-errol@users.noreply.github.com>
2025-07-29 03:16:08 +0800

    Update nav-menu.tsx

--- components/navbar/nav-menu.tsx
+++ components/navbar/nav-menu.tsx
@@ -21,7 +21,7 @@ export const NavMenu = ({ className, ...props }: NavigationMenuProps) => (
       </NavigationMenuItem>
       <NavigationMenuItem>
         <NavigationMenuLink asChild>
-          <Link href="#experience">Experience</Link>
+          <Link href="#journey">Journey</Link>
         </NavigationMenuLink>
       </NavigationMenuItem>
       <NavigationMenuItem>
